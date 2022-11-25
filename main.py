7741ec6aacf06ae916c69bb5662a728c3ed62884
Roman Pulyakho <pulromans@gmail.com>
2022-11-25 19:48:33 +0300

    fix main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route("https://raytorin.github.io/for_rekruto/", methods=['GET'])
+@app.route("/https://raytorin.github.io/for_rekruto/", methods=['GET'])
 def index():
     name = request.args.get('name')
     message = request.args.get('message')
